8b0bded8c290a23963412d2203e6dad4577f4a78
davidmimay <davidmimay@users.noreply.github.com>
2023-01-26 17:45:46 +0000

    Sync from template@608747ecf48a463745ddebcd39410b0867d2a936

---
+++ src/app/user/signin.directive.ts
@@ -0,0 +1,20 @@
+import { Directive, HostListener, Optional } from '@angular/core';
+import { Auth, GoogleAuthProvider, signInWithRedirect } from '@angular/fire/auth';
+
+@Directive({
+  selector: '[appSignin]'
+})
+export class SigninDirective {
+
+  constructor(@Optional() private auth: Auth) {}
+
+  @HostListener('click') onclick() {
+    // ℹ️ https://firebase.google.com/docs/auth/web/google-signin#web-version-9_4
+    const provider = new GoogleAuthProvider()
+    // Ask user for google data access
+    provider.addScope('https://www.googleapis.com/auth/youtube.readonly')
+    // provider.addScope('https://www.googleapis.com/auth/calendar');
+    return signInWithRedirect(this.auth, new GoogleAuthProvider());
+  }
+
+}
