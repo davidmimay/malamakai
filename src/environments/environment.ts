02c9decf00105b6bd9fe48a2011197a31dd55b87
davidmimay <davidmimay@users.noreply.github.com>
2023-02-05 17:43:52 +0000

    Sync from template@34d81f2e18b75b639a84f25d8ff9e552156a48e8

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,18 +1,19 @@
 export const environment = {
   firebase: {
-    apiKey: 'AIzaSyDpcqDZQXZD_K3titfTXGHCcJssHUNnE_0',
-    authDomain: 'malamakai.firebaseapp.com',
-    projectId: 'malamakai',
-    storageBucket: 'malamakai.appspot.com',
-    messagingSenderId: '198661698484',
-    appId: '1:198661698484:web:e268c4a7fce89f689f78e1',
-    measurementId: 'G-GGSMH1XYJY',
+    projectId: 'nooglerway',
+    apiKey: 'AIzaSyDDdSBTsF8prwi9tyi4QrvHdq6QM3MhWhQ',
+    authDomain: 'nooglerway.firebaseapp.com',
+    storageBucket: 'nooglerway.appspot.com',
+    messagingSenderId: '606908323630',
+    appId: '1:606908323630:web:5e611a2b7ee48ba6c6ccbf',
+    measurementId: 'G-QE3H4MJF54',
     locationId: 'europe-west2',
   },
   google: {
-    blogId: '6664790489593253867',
+    bloggerId: '6664790489593253867',
     youtubeId: 'UCjoXaFxfiYpVQo8BGj_Glsg',
-    authClientId: '198661698484-6p7aoj1mni58qq35p2hu3ve7491h1nkj.apps.googleusercontent.com',
+    playlistId: 'PLaRo6khyiK-WUXXv_bS-AlDvmankTFGhc',
+    authClientId: '920807818168-topig3d8j82hrfg47pittjlsofuqlo8b.apps.googleusercontent.com',
     twitterId: '@david_mimay',
   },
   production: false
