56970990bec4f80e7ae083eb083d9d9cdd4a7240
david mimay <davidmimay@gmail.com>
2023-03-14 11:09:30 +0000

    ↗️ update environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,19 +1,19 @@
 export const environment = {
   firebase: {
-    apiKey: "AIzaSyDpcqDZQXZD_K3titfTXGHCcJssHUNnE_0",
-    authDomain: "malamakai.firebaseapp.com",
-    projectId: "malamakai",
-    storageBucket: "malamakai.appspot.com",
-    messagingSenderId: "198661698484",
-    appId: "1:198661698484:web:e268c4a7fce89f689f78e1",
-    measurementId: "G-GGSMH1XYJY",
+    apiKey: 'AIzaSyAkdpJ-AHGWQ_JfgUQ4MKvWvvixKPMu4VQ',
+    authDomain: 'malamakai.firebaseapp.com',
+    projectId: 'malamakai',
+    storageBucket: 'malamakai.appspot.com',
+    messagingSenderId: '198661698484',
+    appId: '1:198661698484:web:e268c4a7fce89f689f78e1',
+    measurementId: 'G-GGSMH1XYJY',
     locationId: 'us-central1',
   },
   google: {
     bloggerId: '6664790489593253867',
     youtubeId: 'UCjoXaFxfiYpVQo8BGj_Glsg',
     playlistId: 'PLaRo6khyiK-WUXXv_bS-AlDvmankTFGhc',
-    authClientId: '920807818168-topig3d8j82hrfg47pittjlsofuqlo8b.apps.googleusercontent.com',
+    authClientId: '198661698484-ls3mf2csnccflibjb42q9sg6jmolvddm.apps.googleusercontent.com',
     twitterId: '@david_mimay',
   },
   production: true
