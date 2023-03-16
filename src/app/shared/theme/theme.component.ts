38239e5498d1838a0af5e684bc13e780bb0479c2
david mimay <davidmimay@gmail.com>
2023-03-16 11:56:23 +0000

    🌗 theme switcher

---
+++ src/app/shared/theme/theme.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-theme',
+  templateUrl: './theme.component.html',
+  styleUrls: ['./theme.component.scss'],
+})
+export class ThemeComponent {
+
+  icon = 'brightness_4';
+
+  constructor() {
+    // Theme toggle
+    const theme = localStorage.getItem('theme');
+    if (theme) {
+      document.body.classList.add(theme);
+    }
+  }
+
+  toggleTheme(): void {
+    if (document.body.classList.contains('light')) {
+      document.body.classList.replace('light', 'dark');
+      localStorage.setItem('theme', 'dark');
+      this.icon = 'light_mode';
+    } else if (document.body.classList.contains('dark')) {
+      document.body.classList.replace('dark', 'light');
+      localStorage.setItem('theme', 'light');
+      this.icon = 'dark_mode';
+    } else {
+      document.body.classList.add('dark');
+      localStorage.setItem('theme', 'dark');
+      this.icon = 'light_mode';
+    }
+  }
+
+}
