a56560098e9b0e4c2792ec2d3f8d6e94838ef36e
david mimay <davidmimay@gmail.com>
2023-03-22 12:02:50 +0000

    🕹 remote config

--- src/app/home/header/header.component.ts
+++ src/app/home/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { getValue, RemoteConfig, fetchAndActivate } from '@angular/fire/remote-config';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  homeTitle:any;
+  homeSubtitle:any;
+  homeAction:any;
 
+  constructor(remoteConfig: RemoteConfig) {
+    fetchAndActivate(remoteConfig)
+    .then(() => {
+      this.homeTitle = getValue(remoteConfig, 'homeTitle');
+      this.homeSubtitle = getValue(remoteConfig, 'homeSubtitle');
+      this.homeAction = getValue(remoteConfig, 'homeAction');
+    })
+    .catch((err) => {
+      console.log('Error:', err)
+    });  
+  }
 }
